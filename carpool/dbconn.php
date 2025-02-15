bf0a655a5b33cb1334784e385834630b330c91de
n1cole111 <nihung43@gmail.com>
2025-02-15 16:19:20 +0800

    Create dbconn.php

---
+++ carpool/dbconn.php
@@ -0,0 +1,13 @@
+<?php
+    //step1 - creat connection to database
+    $hostname = 'localhost';
+    $user = 'root';
+    $password = '';
+    $database = 'carpool';
+    $conn = mysqli_connect($hostname,$user,$password,$database);
+    if($conn === false){
+      die('Connection Failed!'.mysqli_connect_error());
+    } else {
+      // echo 'Connection established!';
+    }
+    ?>
