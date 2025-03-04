4c8f3b668f8814cacfb4f0348af4243481de19f5
n1cole111 <nihung43@gmail.com>
2025-03-04 22:52:09 +0800

    created user header and navigation

---
+++ carpool/userHeader.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/userHeader.css">
+</head>
+<body>
+    <div class="user-bar">
+        <div class="logo">
+            <img src="logo.png" alt="logo">
+        </div>
+        <div class="logout-button">
+            <button onclick="location.href='logout.php'">Log Out</button>        
+        </div>
+    </div>
+</body>
+</html>
