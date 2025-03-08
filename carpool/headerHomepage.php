86993cc789d07383a65de9c8f5c8a5806aeb3723
ericwzhii <ericong12178608@gmail.com>
2025-03-08 17:11:54 +0800

    header update

--- carpool/headerHomepage.php
+++ carpool/headerHomepage.php
@@ -1,8 +1,9 @@
 <?php
-    include("dbconn.php");
+include("dbconn.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,27 +12,30 @@
 
     <!-- <title>headerHomepage</title> -->
 </head>
+
 <body>
+    <?php
+    $currentPage = basename($_SERVER['PHP_SELF']);
+    ?>
+
     <div class="navbar">
         <div class="logo"><img src="image/logo.png" alt="logo"></div>
-        <ul class = "navlinks">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Passenger</a></li>
-            <li><a href="#">Driver</a></li>
-            <li><a href="#">News</a></li>
-            <li class="dropdown">
-                <a href="#">Support</a>
-                <ul class="dropdown-content">
-                    <li><a href="">FAQ</a></li>
-                </ul>   
-            </li>
+        <ul class="navlinks">
+            <li><a href="homepage.php" class="<?= ($currentPage == 'homepage.php') ? 'active' : '' ?>">Home</a></li>
+            <li><a href="#" class="<?= ($currentPage == 'about.php') ? 'active' : '' ?>">About Us</a></li>
+            <li><a href="passenger/passengerPage.php"
+                    class="<?= ($currentPage == 'passengerPage.php') ? 'active' : '' ?>">Passenger</a></li>
+            <li><a href="driverRegistration.php"
+                    class="<?= ($currentPage == 'driverRegistration.php') ? 'active' : '' ?>">Driver</a></li>
+            <li><a href="#" class="<?= ($currentPage == 'news.php') ? 'active' : '' ?>">News</a></li>
+            <li><a href="#" class="<?= ($currentPage == 'support.php') ? 'active' : '' ?>">Support</a></li>
         </ul>
         <div class="buttons">
-            <button class="login">Login</button>
-            <button class="signup">Sign Up</button>
+            <a href="loginpage.php"><button class="login">Login</button></a>
+            <a href="loginpage.php?action=signup"><button class="signup">Sign Up</button></a>
         </div>
     </div>
-    <!-- for home page -->
+
 </body>
+
 </html>
