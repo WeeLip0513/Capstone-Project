5a85973d23d77ce7381809822bbda8132d564814
WeeLip0513 <weelip0513@gmail.com>
2025-03-12 13:24:16 +0800

    map and ride status update

--- carpool/driver/ridePage.php
+++ carpool/driver/ridePage.php
@@ -4,6 +4,7 @@ include("../dbconn.php");
 include("../userHeader.php");
 
 $ride_id = 118;
+$_SESSION['rideID'] = $ride_id;
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -47,6 +48,7 @@ if (!$drop_off) {
 
 // Pass PHP values to JavaScript
 $drop_off_json = json_encode($drop_off);
+$pick_up_json = json_encode($pick_up);
 $ride_details_json = json_encode([
   "pick_up_point" => $pick_up['name'],
   "drop_off_point" => $drop_off['name'],
@@ -64,15 +66,27 @@ $ride_details_json = json_encode([
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Real-time Route</title>
   <link rel="stylesheet" href="../css/ridePage/ridePage.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 
 <body>
   <div class="progress-container">
-    <div class="status" id="waiting">Waiting</div>
-    <div class="status" id="heading">Heading to Destination</div>
-    <div class="status" id="reached">Reached</div>
-    <div class="progress-line"></div>
-    <div class="progress-line-active" id="progress-bar"></div>
+    <div id="progress0" class="progress">
+      <div class="status" id="status0" style="visibility:hidden;">Ride Started</div>
+      <div class="progressLine" id="progressLine0"></div>
+    </div>
+    <div id="progress1" class="progress">
+      <div class="status" id="status1" style="visibility:hidden;">Heading to Pick Up</div>
+      <div class="progressLine" id="progressLine1"></div>
+    </div>
+    <div id="progress2" class="progress">
+      <div class="status" id="status2" style="visibility:hidden;">Heading to Destination</div>
+      <div class="progressLine" id="progressLine2"></div>
+    </div>
+    <div id="progress3" class="progress">
+      <div class="status" id="status3" style="visibility:hidden;">Reached</div>
+      <div class="progressLine" id="progressLine3"></div>
+    </div>
   </div>
 
   <div class="content">
@@ -82,6 +96,7 @@ $ride_details_json = json_encode([
 
   <!-- Pass PHP data to JS -->
   <script id="dropOffData" type="application/json"><?php echo $drop_off_json; ?></script>
+  <script id="pickUpData" type="application/json"><?php echo $pick_up_json; ?></script>
   <script id="rideDetailsData" type="application/json"><?php echo $ride_details_json; ?></script>
 
   <!-- Load JS first -->
